062b5be36ec7229373405cd9267254d52dcc72f7
chuynor <st4083@utr.edu.mx>
2025-12-01 10:23:36 -0600

    crear modelo con control de inventario. También se agregó 'pieza' a unidades

--- src/models/ingredientes.model.js
+++ src/models/ingredientes.model.js
@@ -1,11 +1,29 @@
 import mongoose from 'mongoose';
 
 const ingredienteSchema = new mongoose.Schema({
-  nombre: { type: String, required: true, trim: true, unique: true },
-  // unidad solo permite litros (l) o kilogramos (kg)
-  unidad: { type: String, enum: ['kg', 'l'], required: true, default: 'kg' },
-  // cantidad en la unidad indicada (ej: 2.5 -> 2.5 kg o 1.5 -> 1.5 l)
-  cantidad: { type: Number, required: true, min: 0 },
+  nombre: { 
+    type: String, 
+    required: true, 
+    trim: true, 
+    unique: true,
+    lowercase: true 
+  },
+  unidad: { 
+    type: String, 
+    enum: ['kg', 'l', 'pieza'], 
+    required: true, 
+    default: 'kg' 
+  },
+  cantidad: { 
+    type: Number, 
+    required: true, 
+    min: 0 
+  },
+  stockMinimo: { 
+    type: Number, 
+    min: 0, 
+    default: 5 
+  }
 }, { timestamps: true });
 
 const Ingrediente = mongoose.model('Ingrediente', ingredienteSchema);
